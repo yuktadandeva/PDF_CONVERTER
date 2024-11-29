21bb52a5b4a17b984957155a6ffce75261666f21
Yukta <yuktadandeva@gmail.com>
2024-11-29 22:32:00 +0530

    update

--- scripts/controller/controller.js
+++ scripts/controller/controller.js
@@ -1,1 +1,22 @@
 // handling the features-carousel
+window.addEventListener('DOMContentLoaded',bindEvents);
+
+function bindEvents(){
+    handleCarousel();
+}
+
+function handleCarousel(){
+    // adding event listener
+    const toolDivs = document.querySelectorAll('.tool-carousel-atn');
+    const carouselInner = document.querySelector('.carousel-inner');
+
+    function goToSlide(idx){
+        carouselInner.style.transform = `translateX(-${idx * 100}%)`;
+    }
+
+    toolDivs.forEach((div,idx )=> {
+        div.addEventListener('click',function(){goToSlide(idx)});
+    });
+
+    goToSlide(0);
+}
