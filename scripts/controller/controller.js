c0cebdf8c2bc13f41c70be8502fcde3dd15ad26b
Yukta <yuktadandeva@gmail.com>
2024-11-30 01:10:59 +0530

    update

--- scripts/controller/controller.js
+++ scripts/controller/controller.js
@@ -23,7 +23,17 @@ function handleCarousel(){
 }
 
 function triggerUpload(){
-    document.querySelector('.triggerUpload').addEventListener('click', function(){
+    const uploadButton = document.querySelector('.triggerUpload');
+    const fileInput = document.querySelector('.inputToBeTriggered');
+    uploadButton.addEventListener('click', function(){
         document.querySelector('.inputToBeTriggered').click();
     })
+
+    fileInput.addEventListener('change', function(){
+        if (fileInput.files.length > 0) {
+            // Redirect after file upload
+            window.location.href = ''; // Replace with your desired URL
+        }
+    });
+    
 }
